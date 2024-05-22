e768cac4d4ccb6eeb2236fc62493c596eedb490f
EthanSDD <135563855+EthanSDD@users.noreply.github.com>
2024-05-22 20:54:17 +1000

    Sizing improvements

--- GeoQuiz.py
+++ GeoQuiz.py
@@ -8,33 +8,63 @@ root = CTk()
 root.title("GeoQuiz")
 root.geometry("1000x600")
 root.minsize(1000, 600)
+root.maxsize(1000, 600)
 set_widget_scaling(1.8)
 
 # Initialise Values
 TypedMode = True
 HighScore = 0
+Size = "Medium"
+
+def Selection(Region):
+    if Region == "North America":
+        print("na")
+    elif Region == "South America":
+        print("sa")
+    elif Region == "Europe":
+        print("eu")
+    elif Region == "Asia":
+        print("as")
+    elif Region == "Africa":
+        print("af")
+    elif Region == "Oceania":
+        print("oc")
 
 def BeginQuiz():
     BeginButton.place_forget()
     RegionMenu.place_forget()
     ModeButton.place_forget()
     ScoreLabel.place_forget()
-    ButtonsBG.place(relx=1.0, rely=1.0, anchor='se')
-    SubmitButton.place(relx=0.97, rely=0.74, anchor='se')
-    SkipButton.place(relx=0.97, rely=0.82, anchor='se')
-    NewAttemptButton.place(relx=0.99, rely=0.9, anchor='se')
-    EndQuizButton.place(relx=0.975, rely=0.98, anchor='se')
-    ImageLabel.place(relx=0.5, rely=0.5, anchor='center')
+    if Size == "Small":
+        ButtonsBGS.place(relx=1.0, rely=1.0, anchor="se")
+        SubmitButton.place(relx=0.978, rely=0.83, anchor="se")
+        SkipButton.place(relx=0.978, rely=0.88, anchor="se")
+        NewAttemptButton.place(relx=0.99, rely=0.93, anchor="se")
+        EndQuizButton.place(relx=0.983, rely=0.98, anchor="se")
+    elif Size == "Medium":
+        ButtonsBGM.place(relx=1.0, rely=1.0, anchor="se")
+        SubmitButton.place(relx=0.97, rely=0.74, anchor="se")
+        SkipButton.place(relx=0.97, rely=0.82, anchor="se")
+        NewAttemptButton.place(relx=0.99, rely=0.9, anchor="se")
+        EndQuizButton.place(relx=0.975, rely=0.98, anchor="se")
+    elif Size == "Large":
+        ButtonsBGL.place(relx=1.0, rely=1.0, anchor="se")
+        SubmitButton.place(relx=0.97, rely=0.68, anchor="se")
+        SkipButton.place(relx=0.97, rely=0.78, anchor="se")
+        NewAttemptButton.place(relx=0.99, rely=0.88, anchor="se")
+        EndQuizButton.place(relx=0.975, rely=0.98, anchor="se")
+    ImageLabel.place(relx=0.5, rely=0.5, anchor="center")
     if CurrentMode.get() == "Typed Mode":
-        EntryFieldElement.place(relx=0.5, rely=0.95, anchor='center')
+        EntryFieldElement.place(relx=0.5, rely=0.95, anchor="center")
+        EntryFieldElement.delete(0, END)
     elif CurrentMode.get() == "Select Mode":
-        print("select")
+        Selection(Region)
 
 def GoToMenu():
-    BeginButton.place(relx=0.5, rely=0.35, anchor='center')
-    RegionMenu.place(relx=0.5, rely=0.5, anchor='center')
-    ModeButton.place(relx=0.5, rely=0.6, anchor='center')
-    ScoreLabel.place(relx=0.5, rely=0.7, anchor='center')
+    BeginButton.place(relx=0.5, rely=0.35, anchor="center")
+    RegionMenu.place(relx=0.5, rely=0.5, anchor="center")
+    ModeButton.place(relx=0.5, rely=0.6, anchor="center")
+    ScoreLabel.place(relx=0.5, rely=0.7, anchor="center")
 
 def Submit():
     Answer = EntryFieldElement.get()
@@ -48,7 +78,9 @@ def NewAttempt():
 
 def EndQuiz():
     ImageLabel.place_forget()
-    ButtonsBG.place_forget()
+    ButtonsBGS.place_forget()
+    ButtonsBGM.place_forget()
+    ButtonsBGL.place_forget()
     SubmitButton.place_forget()
     SkipButton.place_forget()
     NewAttemptButton.place_forget()
@@ -81,7 +113,9 @@ def ImportImage(): # Import Image
 ImportImage()
 
 # Quiz Buttons
-ButtonsBG = CTkLabel(root, width=82, height=113, text="", fg_color='#b8b8b8')
+ButtonsBGS = CTkLabel(root, width=95, height=127, text="", fg_color="#b8b8b8")
+ButtonsBGM = CTkLabel(root, width=82, height=110, text="", fg_color="#b8b8b8")
+ButtonsBGL = CTkLabel(root, width=78, height=100, text="", fg_color="#b8b8b8")
 SubmitButton = CTkButton(root, width=50, height=20, text="Submit", command=Submit, font=("Arial", 10), bg_color="#b8b8b8")
 SkipButton = CTkButton(root, width=50, height=20, text="Skip", command=Skip, font=("Arial", 10), bg_color="#b8b8b8")
 NewAttemptButton = CTkButton(root, width=50, height=20, text="New Attempt", command=NewAttempt, font=("Arial", 10), bg_color="#b8b8b8")
@@ -91,20 +125,20 @@ EntryFieldElement = CTkEntry(root, width=200, height=20, placeholder_text="Type 
 GoToMenu()
 
 # Sizing
-def small():
+def Small():
     set_widget_scaling(1.0)
-    root.minsize(1000, 600)
-    root.geometry("1000x600")
+    global Size
+    Size = "Small"
     
-def medium():
+def Medium():
     set_widget_scaling(1.8)
-    root.minsize(1000, 600)
-    root.geometry("1000x600")
+    global Size
+    Size = "Medium"
 
-def large():
+def Large():
     set_widget_scaling(2.4)
-    root.minsize(1000, 600)
-    root.geometry("1000x600")
+    global Size
+    Size = "Large"
 
 # About Popup
 def about():
@@ -134,8 +168,8 @@ menufile.add_command(label="Exit", command=root.destroy)
 menuthem.add_cascade(label="Dark", command=lambda: set_appearance_mode("dark"))
 menuthem.add_cascade(label="Light", command=lambda: set_appearance_mode("light"))
 
-menufont.add_command(label="Small", command=small)
-menufont.add_command(label="Medium", command=medium)
-menufont.add_command(label="Large", command=large)
+menufont.add_command(label="Small", command=Small)
+menufont.add_command(label="Medium", command=Medium)
+menufont.add_command(label="Large", command=Large)
 
 root.mainloop()
