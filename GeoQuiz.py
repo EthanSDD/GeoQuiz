65a00e79a627e105aea6967d1c781ae9542299ba
EthanSDD <135563855+EthanSDD@users.noreply.github.com>
2024-05-22 15:47:45 +1000

    Region image changing

--- GeoQuiz.py
+++ GeoQuiz.py
@@ -12,17 +12,21 @@ set_widget_scaling(1.8)
 
 # Initialise Values
 TypedMode = True
-CountryImage = "France.png"
 HighScore = 0
 
 def BeginQuiz():
-    BeginButton.destroy()
-    RegionMenu.destroy()
-    ModeButton.destroy()
-    ScoreLabel.destroy()
+    BeginButton.place_forget()
+    RegionMenu.place_forget()
+    ModeButton.place_forget()
+    ScoreLabel.place_forget()
+    ButtonsBG.place(relx=1.0, rely=1.0, anchor='se')
+    SubmitButton.place(relx=0.97, rely=0.74, anchor='se')
+    SkipButton.place(relx=0.97, rely=0.82, anchor='se')
+    NewAttemptButton.place(relx=0.99, rely=0.9, anchor='se')
+    EndQuizButton.place(relx=0.975, rely=0.98, anchor='se')
     ImageLabel.place(relx=0.5, rely=0.5, anchor='center')
     if CurrentMode.get() == "Typed Mode":
-        print("typed")
+        EntryFieldElement.place(relx=0.5, rely=0.95, anchor='center')
     elif CurrentMode.get() == "Select Mode":
         print("select")
 
@@ -32,20 +36,57 @@ def GoToMenu():
     ModeButton.place(relx=0.5, rely=0.6, anchor='center')
     ScoreLabel.place(relx=0.5, rely=0.7, anchor='center')
 
-# Import Image
-image_original = Image.open(CountryImage).resize((500, 500))
-image_tk = ImageTk.PhotoImage(image_original)
-ImageLabel = tk.Label(root, text="", image=image_tk)
+def Submit():
+    Answer = EntryFieldElement.get()
+    print(Answer)
+
+def Skip():
+    print()
+
+def NewAttempt():
+    print()
+
+def EndQuiz():
+    ImageLabel.place_forget()
+    ButtonsBG.place_forget()
+    SubmitButton.place_forget()
+    SkipButton.place_forget()
+    NewAttemptButton.place_forget()
+    EndQuizButton.place_forget()
+    EntryFieldElement.place_forget()
+    GoToMenu()
+
+def EntryField():
+    print()
 
 # Menu Buttons
 BeginButton = CTkButton(root, width=250, height=50, text="Begin Quiz", command=BeginQuiz, font=("Arial", 25))
-RegionMenu = CTkOptionMenu(root, width=100, height=25, values=["North America", "South America", "Europe", "Asia", "Africa", "Oceania"], font=("Arial", 10))
+Region = "North America"
+def RegChange(selected_value):
+    global Region
+    Region = selected_value
+    ImportImage()
+RegionMenu = CTkOptionMenu(root, width=100, height=25, values=["North America", "South America", "Europe", "Asia", "Africa", "Oceania"], font=("Arial", 10), command=RegChange)
 CurrentMode = tk.Variable()
 CurrentMode.set("Select Mode")
 ModeButton = CTkSegmentedButton(root, width=100, height=25, values=["Select Mode", "Typed Mode"], font=("Arial", 10), variable=CurrentMode)
 ScoreLabel = CTkLabel(root, text=f"High Score: {HighScore}", font=("Arial", 10))
 
+def ImportImage(): # Import Image
+    image_original = Image.open(f"{Region}.png").resize((500, 500))
+    image_tk = ImageTk.PhotoImage(image_original)
+    global ImageLabel
+    ImageLabel = tk.Label(root, text="", image=image_tk)
+    ImageLabel.image = image_tk
+ImportImage()
+
 # Quiz Buttons
+ButtonsBG = CTkLabel(root, width=82, height=113, text="", fg_color='#b8b8b8')
+SubmitButton = CTkButton(root, width=50, height=20, text="Submit", command=Submit, font=("Arial", 10), bg_color="#b8b8b8")
+SkipButton = CTkButton(root, width=50, height=20, text="Skip", command=Skip, font=("Arial", 10), bg_color="#b8b8b8")
+NewAttemptButton = CTkButton(root, width=50, height=20, text="New Attempt", command=NewAttempt, font=("Arial", 10), bg_color="#b8b8b8")
+EndQuizButton = CTkButton(root, width=50, height=20, text="End Quiz", command=EndQuiz, font=("Arial", 10), bg_color="#b8b8b8")
+EntryFieldElement = CTkEntry(root, width=200, height=20, placeholder_text="Type answer here")
 
 GoToMenu()
 
